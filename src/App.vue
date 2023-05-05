8d197bdbc382b2053e138628c8d9d10cea6fa468
Mazaev Andrey <mazaev2000@gmail.com>
2023-05-05 21:17:35 +0300

    feat: update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
-<script setup lang="ts">
+<script setup>
+import { ref } from 'vue'
+import Card from './components/Card.vue';
+import CardSearch from './components/CardSearch.vue'
+import cardData from './composables/cardData'
+
+
+const searchTerm = ref('');
+const handleSearch = (value) => {
+  searchTerm.value = value;
+};
 
 </script>
 
 <template>
-  <div>
-
+  <div class="container">
+    <div class="search">
+      <CardSearch @search="handleSearch" />
+    </div>
+    <div class="cards">
+      <Card :searchTerm="searchTerm" :cardData="cardData" :selectAllCards="selectAllCards" />
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.search {
+  margin-top: 9rem;
+}
+.cards {
+  margin-top: 3.6rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  row-gap: 2.3rem;
+  column-gap: 3.2rem;
+  @media (max-width: 1000px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
