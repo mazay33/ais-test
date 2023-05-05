9343fab25a2e2df220f05c527ba7f392c1cce023
Mazaev Andrey <mazaev2000@gmail.com>
2023-05-05 21:14:38 +0300

    feat: data for cards

---
+++ src/composables/cardData.js
@@ -0,0 +1,59 @@
+export default {
+  data: [
+    {
+      price: "7 733 300",
+      property_type: "Квартира",
+      seller: "Уступка от юр. лица",
+      residential_complex: "Чистое Небо",
+      building_number: "10",
+      completion_quarter: "III кв.2022г.",
+      room_number: "62",
+      total_sq_m: "234.38",
+      number_of_rooms: "1",
+      floor: "7",
+      address:"Лен. область, Всеволожский район,д. Кудрово, ул. Столичная, д. 5, к. 1",
+      added_date: "21/11/2020",
+      isSelected:false
+    },
+    {
+      price: "7 733 300",
+      seller: "Уступка от физ. лица",
+      residential_complex: "Зеленый квартал на Пулковских высотах",
+      building_number: "10",
+      completion_quarter: "III кв.2022г.",
+      room_number: "62",
+      total_sq_m: "234.38",
+      number_of_rooms: "1",
+      floor: "7",
+      address:"Комендантский пр., уч. 1 Каменка",
+      added_date: "21/11/2020",
+      isSelected:false
+    },
+    {
+      price: "800 300",
+      property_type: "Паркинг",
+      seller: "Забронировано",
+      residential_complex: "Зеленый квартал на Пулковских высотах",
+      building_number: "10",
+      completion_quarter: "III кв.2022г.",
+      object_number:"№ 7-10-2 (ПИБ №68)",
+      total_sq_m: "15",
+      address:"Ленинградская область, Всеволожский район, д. Кудрово, ул. Столичная, д. 5, к. 1",
+      added_date: "21/11/2020",
+      isSelected:false
+    },
+    {
+      price: "800 300",
+      property_type: "Паркинг",
+      seller: "Продано",
+      residential_complex: "Зеленый квартал на Пулковских высотах",
+      building_number: "10",
+      completion_quarter: "III кв.2022г.",
+      object_number:"№ 7-10-2 (ПИБ №68)",
+      total_sq_m: "15",
+      address:"Лен. область, Всеволожский район,д. Кудрово, ул. Столичная, д. 5, к. 1",
+      added_date: "21/11/2020",
+      isSelected:false
+    },
+  ]
+};
